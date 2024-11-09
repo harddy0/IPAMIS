ee6cf2af7d597735f31fce03054ae674e4228013
Leah <leahfayegenson1@gmail.com>
2024-11-09 13:55:13 +0800

    update

--- php/adminEdit.php
+++ php/adminEdit.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic Sidebar Layout</title>
+    <title>Edit Profile</title>
     <link rel="stylesheet" href="../css/adminEdit.css">
 </head>
 <body>
-<div class="sidebar">
+    <div class="sidebar">
         <img src="../images/ctu1.png" alt="Sidebar Background Image" class="background-image">
         <div class="sidebar-content">
             <img src="../images/ctulogo.png" alt="Profile Image">
@@ -36,42 +36,8 @@
     </div>
     
     <div class="dashboard">
-      <h2>Edit Profile</h2>
-        <div class="profile-section">
-            <div class="profile-image">
-                <img src="../images/profile-placeholder.png" alt="Profile Image">
-                <a href="#">Change profile picture</a>
-                <div class="status">
-                    Active status <input type="checkbox" checked>
-                </div>
-            </div>
-            <div class="profile-info">
-                <div class="section-title">Profile Information</div>
-                <div class="form-row">
-                    <input type="text" placeholder="Employee ID Number">
-                    <input type="text" placeholder="User Type">
-                </div>
-                <div class="form-row">
-                    <input type="text" placeholder="First Name">
-                    <input type="text" placeholder="Middle Name">
-                    <input type="text" placeholder="Last Name">
-                </div>
-                <div class="form-row">
-                    <input type="text" placeholder="Campus">
-                    <input type="text" placeholder="Department">
-                    <input type="text" placeholder="Contact Number">
-                </div>
-                <div class="form-row">
-                    <input type="text" placeholder="Username">
-                    <input type="email" placeholder="Email Address">
-                </div>
-                <div class="form-row">
-                    <input type="password" placeholder="New Password">
-                    <input type="password" placeholder="Confirm Password">
-                </div>
-                <button class="update-button">Update</button>
-            </div>
-        </div>
+        <h2>IP Asset Management and Information System</h2>
+        <hr class="custom-line">
     </div>
 </body>
 </html>
