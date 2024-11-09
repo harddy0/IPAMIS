5ec7912d838f7f0a89b1fce2c396f7a554e519d3
Shekinah <shekinah03262004@gmail.com>
2024-11-09 14:14:00 +0800

    update

--- php/adminMu.php
+++ php/adminMu.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Manage Personal Account</title>
-    <link rel="stylesheet" href="../css/adminMpa.css">
+    <title>Admin Manage Users</title>
+    <link rel="stylesheet" href="../css/adminMu.css">
 </head>
 <body>
     <div class="sidebar">
