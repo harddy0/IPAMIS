dfb7564390e346c7ddc5e02c61f48320139b60d9
Kush Aynstayn <njarado2001@gmail.com>
2024-11-09 14:21:40 +0800

    update

--- php/adminMpa.php
+++ php/adminMpa.php
@@ -36,8 +36,12 @@
     </div>
     
     <div class="dashboard">
-        <h2>IP Asset Management and Information System</h2>
-        <hr class="custom-line">
+        <div class="header">
+            <h2>IP Asset Management and Information System</h2>
+            <img src="../images/pilipinaslogo.png" alt="Logo" class="header-logo">
+        </div>
+            <hr class="custom-line">
     </div>
+
 </body>
 </html>
