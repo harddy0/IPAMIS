9775d67800ef77b33064a996d06fe61836777b47
Leah <leahfayegenson1@gmail.com>
2024-11-10 11:45:23 +0800

    up

--- php/adminMipa.php
+++ php/adminMipa.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Manage Ip Assests</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/adminMipa.css">
+    <link rel="stylesheet" href="../css/adminMpa.css">
     <link rel="stylesheet" href="../css/dashboard.css">
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/footer.css">
 </head>
 <body>
-<!--hello!-->
+
     <?php include '../includes/dashboard.php'; ?>
     <?php include '../includes/header.php'; ?>
 
--- php/adminVa.php
+++ php/adminVa.php
@@ -3,45 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin View Analytics</title>
-    <link rel="stylesheet" href="../css/adminVa.css">
+    <title>Admin Manage Ip Assests</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/adminMpa.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
 </head>
 <body>
-    <div class="sidebar">
-        <img src="../images/ctu1.png" alt="Sidebar Background Image" class="background-image">
-        <div class="sidebar-content">
-            <img src="../images/ctulogo.png" alt="Profile Image">
-            <div class="title">IPAMIS</div>
-            <div class="admin">ADMIN</div>
-            
-            <div class="menu">
-                <a href="#" class="menu-item active" onclick="window.location.href='adminMpa.php';">
-                    <span class="icon">👤</span> Manage Personal Account
-                </a>
-                <a href="#" class="menu-item" onclick="window.location.href='adminMu.php';">
-                    <span class="icon">⚙️</span> Manage Users
-                </a>
-                <a href="#" class="menu-item" onclick="window.location.href='adminMip.php';">
-                    <span class="icon">💼</span> Manage IP Assets
-                </a>
-                <a href="#" class="menu-item" onclick="window.location.href='adminVa.php';">
-                    <span class="icon">📊</span> View Analytics
-                </a>
-            </div>
 
-            <div class="logout">
-                <span class="icon">🚪</span> Log out
-            </div>
-        </div>
-    </div>
+    <?php include '../includes/dashboard.php'; ?>
+    <?php include '../includes/header.php'; ?>
+
     
-    <div class="dashboard">
-        <div class="header">
-            <h2>IP Asset Management and Information System</h2>
-            <img src="../images/pilipinaslogo.png" alt="Logo" class="header-logo">
-        </div>
-            <hr class="custom-line">
+    <?php include '../includes/footer.php'; ?>
     </div>
+
 
 </body>
 </html>
