91f47ad9edeb4e98222a5d695a9d551527d84ead
Kush Aynstayn <njarado2001@gmail.com>
2024-11-10 12:23:40 +0800

    up

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -1,4 +1,6 @@
-<script src="../javascript/smooth.js"></script>
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']); // Get the name of the current file
+?>
 <div class="sidebar">
         <img src="../images/ctu1.png" alt="Sidebar Background Image" class="background-image">
         <div class="sidebar-content">
@@ -7,21 +9,21 @@
             <div class="admin">ADMIN</div>
             
             <div class="menu">
-                <a href="#" class="menu-item active" data-target="profile-section" onclick="window.location.href='adminMpa.php';">
+                <a href="#" class="menu-item <?php echo $currentPage == 'adminMpa.php' ? 'active' : ''; ?>" onclick="window.location.href='adminMpa.php';">
                     <span class="icon">👤</span>
                     <div class="text ml-1">Manage Personal Account</div>
                 </a>
-                <a href="#" class="menu-item" data-target="manage-users-section" onclick="window.location.href='adminMu.php';">
+                <a href="#" class="menu-item <?php echo $currentPage == 'adminMu.php' ? 'active' : ''; ?>" onclick="window.location.href='adminMu.php';">
                     <span class="icon">⚙️</span>
                     <div class="text">Manage Users</div>
                 </a>
-                <a href="#" class="menu-item" data-target="manage-ip-assets-section" onclick="window.location.href='adminMip.php';">
+                <a href="#" class="menu-item <?php echo $currentPage == 'adminMip.php' ? 'active' : ''; ?>" onclick="window.location.href='adminMip.php';">
                     <span class="icon">💼</span> 
                     <div class="text">Manage IP Assets</div>
                 </a>
-                <a href="#" class="menu-item" data-target="view-analytics-section" onclick="window.location.href='adminVa.php';">
+                <a href="#" class="menu-item <?php echo $currentPage == 'adminVa.php' ? 'active' : ''; ?>" onclick="window.location.href='adminVa.php';">
                     <span class="icon">📊</span>
-                    <div class="text">Manage IP Assets</div>
+                    <div class="text">View Analytics</div>
                 </a>
             </div>
 
