2544b63ea010d4ae2659dc7a82dfad7fe38bf767
Shekinah <shekinah03262004@gmail.com>
2024-11-10 11:07:01 +0800

    update

--- php/adminMu.php
+++ php/adminMu.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Manage IP Assets</title>
+    <title>Admin Manage Users</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/adminMu.css">
     <link rel="stylesheet" href="../css/dashboard.css">
