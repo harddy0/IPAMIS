4bca0f483f5001c1909ac2d37892f9bd3a527f5d
Shekinah <shekinah03262004@gmail.com>
2024-11-10 12:56:08 +0800

    update

--- php/adminVa.php
+++ php/adminVa.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Manage Ip Assests</title>
+    <title>Admin View Analytics</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/adminMpa.css">
     <link rel="stylesheet" href="../css/dashboard.css">
