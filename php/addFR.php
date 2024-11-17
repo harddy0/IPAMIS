e64ed4086c756e50abab2bb68a1b9a6c432656ee
Kush Aynstayn <njarado2001@gmail.com>
2024-11-17 22:59:45 +0800

    H

--- php/addFR.php
+++ php/addFR.php
@@ -90,7 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file']) && isset($_P
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin View Analytics</title>
+    <title>Add Formality Report</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="icon" href="../images/ctulogo.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/adminVa.css">
