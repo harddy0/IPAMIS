a0ee694e375b02f9e92d6393a6ba072012ef2d94
Shekinah <shekinah03262004@gmail.com>
2024-11-18 16:24:31 +0800

    update view SOA design

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "leahfaye";
+$password = "@#shekinah2004@#";
 $dbname = "ipamis";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
