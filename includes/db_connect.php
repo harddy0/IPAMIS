6d64cc3c6b356742780a0ed2bac0e94639b9d266
harddy0 <kingstonharddycanales@gmail.com>
2024-11-19 22:32:58 +0800

    sdfg

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,11 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-<<<<<<< HEAD
 $password = "root";
-=======
-$password = "leahfaye";
->>>>>>> a38f70f24623a44c58665903208b36ce8ed93b3f
 $dbname = "ipamis";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
