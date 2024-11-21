8f3c4ffacec28428bf9751618dd9bf635bbef27d
harddy0 <kingstonharddycanales@gmail.com>
2024-11-21 12:04:09 +0800

    sdfes

---
+++ php/modal.php
@@ -0,0 +1,12 @@
+<!-- modal.php -->
+<div id="modal" class="fixed inset-0 flex items-center justify-center z-50 hidden">
+    <div class="bg-black bg-opacity-50 absolute inset-0"></div>
+    <div class="bg-white p-6 rounded-lg z-10 max-w-sm w-full">
+        <p id="modal-message" class="text-gray-800 text-lg"></p>
+        <div class="mt-4 flex justify-end">
+            <button id="modal-close" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                OK
+            </button>
+        </div>
+    </div>
+</div>
