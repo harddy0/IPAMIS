ad9b515ab8ba33de6c2c1983fd20e30c99e0144d
harddy0 <kingstonharddycanales@gmail.com>
2024-11-21 12:40:20 +0800

    dfg

--- php/viewFR.php
+++ php/viewFR.php
@@ -1,5 +1,5 @@
 <?php
-// manage_formality.php
+// viewFR.php
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
--- php/viewOR.php
+++ php/viewOR.php
@@ -1,5 +1,5 @@
 <?php
-// manage_or.php
+// viewOR.php
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
